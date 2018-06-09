633b298de4c80daad07d78031eadb093313cc159
[akolbaev] <[azamat@na.edu]>
2018-06-09 13:53:50 -0500

    added run.py

---
+++ run.py
@@ -0,0 +1,47 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import pandas as pd
+from flask import Flask
+import os
+from numbers import Number
+import xlrd
+
+#url ='https://github.com/akolbaev/excel/blob/master/Purchase_Order_History.xlsx?raw=true'
+#df = pd.read_excel(url, sheet_name='Sheet1')
+
+#df = pd.read_excel(('https://github.com/akolbaev/excel/blob/master/Purchase_Order_History.xlsx'), sheetname='Sheet 1')
+
+server = Flask(__name__)
+server.secret_key = os.environ.get('secret_key', 'secret')
+app = dash.Dash(name = __name__, server = server)
+app.config.supress_callback_exceptions = True
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+    dcc.Graph(
+        id='example-graph',    
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    ),
+    dcc.Input(id='my-id', value='initial value', type="text"),
+    html.Div(id='my-div')
+])
+
+@app.callback(
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-id', component_property='value')]
+)
+def update_output_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value)
